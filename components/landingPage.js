d2cad2205bf379dd178281e31c4090cc9b9b9ed3
monero <forpetessake@protonmail.com>
2018-11-12 16:13:13 +0000

    add more validation to landing page

--- components/landingPage.js
+++ components/landingPage.js
@@ -2,111 +2,182 @@ import styles from '../styles/style.scss'
 import { Component } from 'react';
 import DebugButton from './debugButton';
 import Router from 'next/router';
+import axios from 'axios';
+
+const warningPass = "Password needs to 8 or more characters and only letters or numbers";
+const warningName = "Name needs to 1 or more characters and only letters or numbers"
+const warningBoth = warningName + "," + warningPass;
 
 class LandingPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            register:true,
-            name:'',
-            pass:'',
-            email:'',
-            validPass:false,
-            validName:false,
-        };
-        this.handleToggle = this.handleToggle.bind(this);
-        this.debugHandler = this.debugHandler.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-        this.handleRegister = this.handleRegister.bind(this);
-        this.handleChangeLogin = this.handleChangeLogin.bind(this);
-        this.handleChangeReg = this.handleChangeReg.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      register: true,
+      name: '',
+      pass: '',
+      email: '',
+      validPass: false,
+      validName: false,
+      warning: '',
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.debugHandler = this.debugHandler.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
+    this.handleChangeLogin = this.handleChangeLogin.bind(this);
+    this.handleChangeReg = this.handleChangeReg.bind(this);
+    this.checkWarnings = this.checkWarnings.bind(this);
 
 
-    }
-    handleToggle(){
-        console.log('reg=',this.state.register);
-        this.setState({register:!this.state.register})
-       // this.forceUpdate();
-    }
-    isValid(str){
-      return /^\w+$/.test(str);
-    }
-    validatePass(pass) {
+  }
+  handleToggle() {
+    console.log('reg=', this.state.register);
+    this.setState({ register: !this.state.register,warning:'' })
+    // this.forceUpdate();
+  }
+  isValid(str) {
+    return /^\w+$/.test(str);
+  }
+  validatePass(pass) {
 
-      if (pass > 7 && isValid(pass) === true){
-        this.setState({validPass:true});
-        return true;
-      }else {
-        this.setState({validPass:false});
-        return false;
+    if (pass.length > 7 && this.isValid(pass) === true) {
+      this.setState({ validPass: true });
+    } else {
+      this.setState({ validPass: false });
+    }
+
+  }
+  validateName(name) {
+
+    //checkname should only be letters and numbers only
+    if (this.isValid(name) === true && name.length > 0) {
+      this.setState({ validName: true });
+    } else {
+      this.setState({ validName: false });
+    }
+    //checkpassword - should be at least 8 characters long
+  }
+  checkWarnings() {
+    if (this.state.validName !== true && this.state.validPass !== true) {
+      this.setState({ warning: warningBoth });
+      return;
+    } else if (this.state.validName !== true) {
+      this.setState({ warning: warningName });
+      return;
+    } else if (this.state.validPass !== true) {
+      this.setState({ warning: warningPass });
+      return;
+    } else {
+      this.setState({ warning: '' });
+    }
+
+  }
+  handleLogin(event) {
+    event.preventDefault();
+    console.log('In handleLogin');
+    this.checkWarnings()
+    console.log('here here');
+
+
+  }
+  handleRegister(event) {
+    event.preventDefault();
+    console.log('In handleRegister');
+    this.checkWarnings();
+      console.log('here here');
+
+  }
+  handleChangeReg(event) {
+    console.log('etv=', event.target.value);
+    console.log('etn=', event.target.name);
+    let name = event.target.name + "";
+    if (name === "name") {
+      this.validateName(event.target.value);
+    } else if(name === "pass") {
+      this.validatePass(event.target.value);
+    }
+    //always set the name whatever
+    this.setState({ [name]: event.target.value })
+
+  }
+  handleChangeLogin(event) {
+    console.log('etv=', event.target.value);
+    console.log('etn=', event.target.name);
+    let name = event.target.name + "";
+    if (name === "name") {
+      this.validateName(event.target.value);
+    } else if(name === "pass") {
+      this.validatePass(event.target.value);
+    }
+
+    this.setState({ [name]: event.target.value })
+
+
+  }
+  sendReg() {
+    axios.post('/register', {
+      data:{
+        name:this.state.name,
+        email:this.state.email,
+        pass:this.state.pass,
       }
-
-    }
-    validateName(name) {
-      
-      //checkname should only be letters and numbers only
-      if (isValid(name) === true && name > 0){
-        this.setState({validName:true});
-        return true;
-      }else {
-        this.setState({validName:false});
-        return false;
+    })
+    .then(response => {
+        console.log('login resp=',response.data)
+       // this.setState({ result: response.data.join("") })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  sendLogin() {
+     axios.post('/login', {
+      data:{
+        name:this.state.name,
+        pass:this.state.pass,
       }
-      //checkpassword - should be at least 8 characters long
-    }
-    handleLogin(event){
-      event.preventDefault();
+    })
+    .then(response => {
+        console.log('login resp=',response.data)
+   //     this.setState({ result: response.data.join("") })
+      })
+      .catch(error => {
+        console.log(error);
+      });
+ 
+  }
+  debugHandler() {
 
-    }
-    handleRegister(event){
-      event.preventDefault();
-    }
-    handleChangeReg(event){
-      console.log('etv=',event.target.value);
-      console.log('etn=',event.target.name);
-      let name = event.target.name + "";
-      this.validateReg(name);
-      this.setState({[name]: event.target.value})
+    console.log('state=', this.state);
+  }
 
-    }
-    handleChangeLogin(event){
-      console.log('etv=',event.target.value);
-      console.log('etn=',event.target.name);
-      let name = event.target.name;
-      this.setState({[name]:event.target.value})
+  render() {
+    return (
+      <div>
 
-
-    }
-    debugHandler(){
-
-        console.log('state=',this.state);
-    }
-
-    render() {
-        return (
-            <div>
-            
-                <div className="login-page">
-                    <div className="form">
-                    {!this.state.register ?
-                        <form onSubmit={this.handleRegister} className="register-form">
-                            <input name="name" onChange={e => this.handleChangeReg(e)} value={this.state.name} type="text" placeholder="name" />
-                            <input name="email" onChange={e => this.handleChangeReg(e)} value={this.state.email} type="email" placeholder="email address" />
-                            <input name="pass" onChange={e => this.handleChangeReg(e)} value={this.state.pass} type="password" placeholder="password" />
-                            <button disabled={!this.state.valid}>create</button>
-                            <p className="message">Already registered? <a href='#' onClick={this.handleToggle}>Sign In</a></p>
-                        </form> :
-                        <form onSubmit={this.handleLogin} className="login-form">
-                            <input name="name" onChange={e => this.handleChangeLogin(e)} value={this.state.name} type="text" placeholder="username" />
-                            <input name="pass" onChange={e => this.handleChangeLogin(e)} value={this.state.pass} type="password" placeholder="password" />
-                            <button disabled={!this.state.valid}>login</button>
-                            <p className="message">Not registered? <a href='#' onClick={this.handleToggle}>Create an account</a></p>
-                        </form>
-                       }
-                    </div>
-                </div>
-                <DebugButton name="test" handler={this.debugHandler}/>
-                <style jsx>{`
+        <div className="login-page">
+          <div className="form">
+            {!this.state.register ?
+              <form onSubmit={this.handleRegister} className="register-form">
+                <input name="name" onChange={e => this.handleChangeReg(e)} value={this.state.name} type="text" placeholder="name" />
+                <input required={true} name="email" onChange={e => this.handleChangeReg(e)} value={this.state.email} type="email" placeholder="email address" />
+                <input name="pass" onChange={e => this.handleChangeReg(e)} value={this.state.pass} type="password" placeholder="password" />
+                <button disabled={false}>create</button>
+                <p className="message">Already registered? <a href='#' onClick={this.handleToggle}>Sign In</a></p>
+                <p className="warning">{this.state.warning}</p>
+              </form> :
+              <form onSubmit={this.handleLogin} className="login-form">
+                <input name="name" onChange={e => this.handleChangeLogin(e)} value={this.state.name} type="text" placeholder="username" />
+                <input name="pass" onChange={e => this.handleChangeLogin(e)} value={this.state.pass} type="password" placeholder="password" />
+                <button disabled={false}>login</button>
+                <p className="message">Not registered? <a href='#' onClick={this.handleToggle}>Create an account</a></p>
+                <p className="warning">{this.state.warning}</p>
+              </form>
+            }
+          </div>
+        </div>
+        <DebugButton name="test" handler={this.debugHandler} />
+        <style jsx>{`
 @import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
 .login-page {
@@ -156,6 +227,12 @@ class LandingPage extends Component {
   margin: 15px 0 0;
   color: #b3b3b3;
   font-size: 12px;
+}
+.form .warning {
+  margin: 15px 0 0;
+  color: #ff0000;
+  font-size: 12px;
+ 
 }
 .form .message a {
   color: #4CAF50;
@@ -209,9 +286,9 @@ body {
   -moz-osx-font-smoothing: grayscale;      
 }
 `}</style>
-            </div>
-        
-        )
-}
+      </div>
+
+    )
+  }
 }
 export default LandingPage;
